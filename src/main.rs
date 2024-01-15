95b1236a1504ba8b75573b48a19f1176e230fe5c
Ross Byrne <ross.byrne@outlook.ie>
2024-01-15 19:18:51 +0000

    Extracted player logic out to PlayerPlugin

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,18 @@
+use crate::player::*;
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Player;
-
-#[derive(Component)]
-struct Shield;
+pub mod player;
 
 #[derive(Component)]
 struct Name(String);
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, PlayerPlugin))
         .add_systems(Startup, setup)
-        .add_systems(Update, sprite_movement)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-
-    // Add player sprite
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("player.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
-            ..default()
-        },
-        Player,
-    ));
-
-    // Add player shield and scale up sprite
-    let mut shield_transform: Transform = Transform::from_xyz(100., 0., 0.);
-    shield_transform.scale = Vec3::new(1.3, 1.3, 0.);
-
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("shield.png"),
-            transform: shield_transform,
-            ..default()
-        },
-        Player,
-        Shield,
-    ));
-}
-
-/// The sprite is animated by changing its translation depending on the time that has passed since
-/// the last frame.
-fn sprite_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    time: Res<Time>,
-    mut sprite_position: Query<(&mut Transform, With<Player>)>,
-) {
-    for (mut transform, ()) in &mut sprite_position {
-        if keyboard_input.pressed(KeyCode::W) {
-            transform.translation.y += 150. * time.delta_seconds();
-        }
-
-        if keyboard_input.pressed(KeyCode::S) {
-            transform.translation.y -= 150. * time.delta_seconds();
-        }
-
-        if keyboard_input.pressed(KeyCode::A) {
-            transform.translation.x -= 150. * time.delta_seconds();
-        }
-
-        if keyboard_input.pressed(KeyCode::D) {
-            transform.translation.x += 150. * time.delta_seconds();
-        }
-    }
+fn setup(mut _commands: Commands, _asset_server: Res<AssetServer>) {
+    println!("Setting up main")
 }
---
+++ src/player.rs
@@ -0,0 +1,70 @@
+use bevy::prelude::*;
+
+pub struct PlayerPlugin;
+
+#[derive(Component)]
+pub struct Player;
+
+#[derive(Component)]
+pub struct Shield;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup)
+            .add_systems(Update, player_movement);
+    }
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Add player sprite
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("circle.png"),
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..default()
+        },
+        Player,
+    ));
+
+    // Add player shield and scale up sprite
+    let mut shield_transform: Transform = Transform::from_xyz(100., 0., 0.);
+    shield_transform.scale = Vec3::new(1.3, 1.3, 0.);
+
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("ring.png"),
+            transform: shield_transform,
+            ..default()
+        },
+        Player,
+        Shield,
+    ));
+}
+
+/// The sprite is animated by changing its translation depending on the time that has passed since
+/// the last frame.
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut sprite_position: Query<(&mut Transform, With<Player>)>,
+) {
+    for (mut transform, ()) in &mut sprite_position {
+        if keyboard_input.pressed(KeyCode::W) {
+            transform.translation.y += 180. * time.delta_seconds();
+        }
+
+        if keyboard_input.pressed(KeyCode::S) {
+            transform.translation.y -= 180. * time.delta_seconds();
+        }
+
+        if keyboard_input.pressed(KeyCode::A) {
+            transform.translation.x -= 180. * time.delta_seconds();
+        }
+
+        if keyboard_input.pressed(KeyCode::D) {
+            transform.translation.x += 180. * time.delta_seconds();
+        }
+    }
+}
