564f5731bc47612109137c47a1fbfedcaa86d6cc
Ross Byrne <ross.byrne@outlook.ie>
2024-01-17 21:57:47 +0000

    Working on start page ui

--- src/ui.rs
+++ src/ui.rs
@@ -1,11 +1,15 @@
-use bevy::prelude::*;
+use bevy::{app::AppExit, prelude::*};
 
 pub struct UIPlugin;
+
+#[derive(Component)]
+pub struct ExitButton;
 
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
-            .add_systems(Startup, (spawn_box, spawn_text));
+            .add_systems(Startup, spawn_buttons)
+            .add_systems(Update, (button_system, on_click_exit).chain());
     }
 }
 
@@ -13,54 +17,141 @@ fn setup(mut _commands: Commands, _asset_server: Res<AssetServer>) {
     println!("Init game UI");
 }
 
-fn spawn_box(mut commands: Commands) {
-    let container = NodeBundle {
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const PRESSED_BUTTON: Color = Color::rgb(0.15, 0.15, 0.6);
+const HOVERED_BUTTON: Color = Color::rgb(0.15, 0.3, 0.15);
+
+const BUTTON_HEIGHT: Val = Val::Px(65.0);
+const BUTTON_WIDTH: Val = Val::Px(240.0);
+
+fn get_button_style() -> Style {
+    return Style {
+        width: BUTTON_WIDTH,
+        height: BUTTON_HEIGHT,
+        margin: UiRect::all(Val::Px(4.0)),
+        border: UiRect::all(Val::Px(1.0)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        ..default()
+    };
+}
+fn spawn_buttons(mut commands: Commands) {
+    let container_node = NodeBundle {
         style: Style {
+            // display: Display::Flex,
+            flex_direction: FlexDirection::Column,
             width: Val::Percent(100.0),
             height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
             justify_content: JustifyContent::Center,
-            align_content: AlignContent::Start,
             ..default()
         },
         ..default()
     };
 
-    let square = NodeBundle {
-        style: Style {
-            width: Val::Px(100.),
-            height: Val::Px(80.),
-            border: UiRect::all(Val::Px(2.)),
-            ..default()
-        },
-        background_color: Color::rgb(0.65, 0.65, 0.65).into(),
+    let button_node = ButtonBundle {
+        style: get_button_style(),
+        border_color: BorderColor(Color::BLACK),
+        background_color: NORMAL_BUTTON.into(),
         ..default()
     };
 
-    let parent = commands.spawn(container).id();
-    let child = commands.spawn(square).id();
+    let button_text_node = TextBundle::from_section(
+        "Start Game",
+        TextStyle {
+            font_size: 40.0,
+            color: Color::rgb(0.9, 0.9, 0.9),
+            ..default()
+        },
+    );
 
-    commands.entity(parent).push_children(&[child]);
+    let container = commands.spawn(container_node).id();
+    let button = commands.spawn(button_node).id();
+    let button_text = commands.spawn(button_text_node).id();
+
+    commands.entity(button).push_children(&[button_text]);
+    commands.entity(container).push_children(&[button]);
+
+    let button_node = ButtonBundle {
+        style: get_button_style(),
+        border_color: BorderColor(Color::BLACK),
+        background_color: NORMAL_BUTTON.into(),
+        ..default()
+    };
+
+    let button_text_node = TextBundle::from_section(
+        "Settings",
+        TextStyle {
+            font_size: 40.0,
+            color: Color::rgb(0.9, 0.9, 0.9),
+            ..default()
+        },
+    );
+
+    let button = commands.spawn(button_node).id();
+    let button_text = commands.spawn(button_text_node).id();
+
+    commands.entity(button).push_children(&[button_text]);
+    commands.entity(container).push_children(&[button]);
+
+    let button_node = ButtonBundle {
+        style: get_button_style(),
+        border_color: BorderColor(Color::BLACK),
+        background_color: NORMAL_BUTTON.into(),
+        ..default()
+    };
+
+    let button_text_node = TextBundle::from_section(
+        "Quit",
+        TextStyle {
+            font_size: 40.0,
+            color: Color::rgb(0.9, 0.9, 0.9),
+            ..default()
+        },
+    );
+
+    let button = commands.spawn((button_node, ExitButton)).id();
+    let button_text = commands.spawn(button_text_node).id();
+
+    commands.entity(button).push_children(&[button_text]);
+    commands.entity(container).push_children(&[button]);
 }
 
-fn spawn_text(mut commands: Commands) {
-    let text = "Hello world!";
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, &mut BorderColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color, mut border_color) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = Color::RED;
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = Color::BLACK;
+            }
+        }
+    }
+}
 
-    commands.spawn(
-        TextBundle::from_section(
-            text,
-            TextStyle {
-                font_size: 100.0,
-                color: Color::WHITE,
-                ..default()
-            },
-        ) // Set the alignment of the Text
-        .with_text_alignment(TextAlignment::Center)
-        // Set the style of the TextBundle itself.
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            bottom: Val::Px(5.0),
-            right: Val::Px(5.0),
-            ..default()
-        })
-    );
+fn on_click_exit(
+    mut app_exit_events: EventWriter<AppExit>,
+    mut interaction_query: Query<&Interaction, (Changed<Interaction>, With<ExitButton>)>,
+) {
+    for interaction in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                app_exit_events.send(AppExit);
+            }
+            Interaction::Hovered => {}
+            Interaction::None => {}
+        }
+    }
 }
