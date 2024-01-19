4203ed6b4aecb2e2176976d5f2ceea49b642dfb5
Ross Byrne <ross.byrne@outlook.ie>
2024-01-19 19:15:08 +0000

    cleaning up code

--- src/ui.rs
+++ src/ui.rs
@@ -3,6 +3,9 @@ use bevy::{app::AppExit, prelude::*};
 pub struct UIPlugin;
 
 #[derive(Component)]
+pub struct StartUI;
+
+#[derive(Component)]
 pub struct ExitButton;
 
 impl Plugin for UIPlugin {
@@ -24,21 +27,39 @@ const HOVERED_BUTTON: Color = Color::rgb(0.15, 0.3, 0.15);
 const BUTTON_HEIGHT: Val = Val::Px(65.0);
 const BUTTON_WIDTH: Val = Val::Px(240.0);
 
-fn get_button_style() -> Style {
-    return Style {
-        width: BUTTON_WIDTH,
-        height: BUTTON_HEIGHT,
-        margin: UiRect::all(Val::Px(4.0)),
-        border: UiRect::all(Val::Px(1.0)),
-        justify_content: JustifyContent::Center,
-        align_items: AlignItems::Center,
+fn get_button_bundle() -> ButtonBundle {
+    return ButtonBundle {
+        style: Style {
+            width: BUTTON_WIDTH,
+            height: BUTTON_HEIGHT,
+            margin: UiRect::all(Val::Px(4.0)),
+            border: UiRect::all(Val::Px(1.0)),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        border_color: BorderColor(Color::BLACK),
+        background_color: NORMAL_BUTTON.into(),
         ..default()
     };
 }
+
+fn get_text_bundle(text: &str) -> TextBundle {
+    return TextBundle::from_section(
+        text,
+        TextStyle {
+            font_size: 40.0,
+            color: Color::rgb(0.9, 0.9, 0.9),
+            ..default()
+        },
+    );
+}
+
 fn spawn_buttons(mut commands: Commands) {
+    // Create and spawn main UI container
     let container_node = NodeBundle {
         style: Style {
-            // display: Display::Flex,
+            display: Display::Flex,
             flex_direction: FlexDirection::Column,
             width: Val::Percent(100.0),
             height: Val::Percent(100.0),
@@ -48,71 +69,27 @@ fn spawn_buttons(mut commands: Commands) {
         },
         ..default()
     };
+    let container = commands.spawn((container_node, StartUI)).id();
 
-    let button_node = ButtonBundle {
-        style: get_button_style(),
-        border_color: BorderColor(Color::BLACK),
-        background_color: NORMAL_BUTTON.into(),
-        ..default()
-    };
-
-    let button_text_node = TextBundle::from_section(
-        "Start Game",
-        TextStyle {
-            font_size: 40.0,
-            color: Color::rgb(0.9, 0.9, 0.9),
-            ..default()
-        },
-    );
-
-    let container = commands.spawn(container_node).id();
-    let button = commands.spawn(button_node).id();
-    let button_text = commands.spawn(button_text_node).id();
-
+    // Create and spawn Start Game Button
+    let button = commands.spawn((get_button_bundle(), StartUI)).id();
+    let button_text = commands
+        .spawn((get_text_bundle("Start Game"), StartUI))
+        .id();
     commands.entity(button).push_children(&[button_text]);
     commands.entity(container).push_children(&[button]);
 
-    let button_node = ButtonBundle {
-        style: get_button_style(),
-        border_color: BorderColor(Color::BLACK),
-        background_color: NORMAL_BUTTON.into(),
-        ..default()
-    };
-
-    let button_text_node = TextBundle::from_section(
-        "Settings",
-        TextStyle {
-            font_size: 40.0,
-            color: Color::rgb(0.9, 0.9, 0.9),
-            ..default()
-        },
-    );
-
-    let button = commands.spawn(button_node).id();
-    let button_text = commands.spawn(button_text_node).id();
-
+    // Create and spawn Settings Button
+    let button = commands.spawn((get_button_bundle(), StartUI)).id();
+    let button_text = commands.spawn((get_text_bundle("Settings"), StartUI)).id();
     commands.entity(button).push_children(&[button_text]);
     commands.entity(container).push_children(&[button]);
 
-    let button_node = ButtonBundle {
-        style: get_button_style(),
-        border_color: BorderColor(Color::BLACK),
-        background_color: NORMAL_BUTTON.into(),
-        ..default()
-    };
-
-    let button_text_node = TextBundle::from_section(
-        "Quit",
-        TextStyle {
-            font_size: 40.0,
-            color: Color::rgb(0.9, 0.9, 0.9),
-            ..default()
-        },
-    );
-
-    let button = commands.spawn((button_node, ExitButton)).id();
-    let button_text = commands.spawn(button_text_node).id();
-
+    // Create and spawn Quit Button
+    let button = commands
+        .spawn((get_button_bundle(), StartUI, ExitButton))
+        .id();
+    let button_text = commands.spawn((get_text_bundle("Quit"), StartUI)).id();
     commands.entity(button).push_children(&[button_text]);
     commands.entity(container).push_children(&[button]);
 }
